0ee9904ed5dcd8de7b4889ffd463a005a086ddb0
Alex <budnikoleksii@gmail.com>
2020-09-16 15:04:41 +0300

    restaurant app default

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducer';
 import App from './components/app';
 
-const store = createStore(reducer);
+import './index.scss';
+
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
+    <App/>
     , document.getElementById('root'));
 
